0a0c881ce868fcf9a5269d8324bd4b596c297403
707g <daandaan707@gmail.com>
2025-01-17 17:46:34 +0900

    수업자료업로드

--- src/main/java/com/korit/servlet_study/entity/BookCategory.java
+++ src/main/java/com/korit/servlet_study/entity/BookCategory.java
@@ -1,3 +1,4 @@
+
 package com.korit.servlet_study.entity;
 
 import lombok.AllArgsConstructor;
@@ -9,10 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class User {
-    private int userId;
-    private String username;
-    private String password;
-    private String name;
-    private String email;
+public class BookCategory {
+    private int categoryId;
+    private String categoryName;
 }
